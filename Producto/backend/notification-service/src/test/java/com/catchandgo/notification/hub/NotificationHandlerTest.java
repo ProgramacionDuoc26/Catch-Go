b339dd5cb095a31ffbfb62d9220c1a1ad49f0472
Miguel Arredondo <xmiguelaav@gmail.com>
2026-06-14 20:53:58 -0400

    fix: remove unnecessary rawtypes warning suppression in NotificationHandlerTest

--- Producto/backend/notification-service/src/test/java/com/catchandgo/notification/hub/NotificationHandlerTest.java
+++ Producto/backend/notification-service/src/test/java/com/catchandgo/notification/hub/NotificationHandlerTest.java
@@ -17,7 +17,7 @@ import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-@SuppressWarnings({"null", "unchecked", "rawtypes"})
+@SuppressWarnings({"null", "unchecked"})
 public class NotificationHandlerTest {
 
     @Mock
