452b695448a1404f14ecbd0de83f9598d68e64df
Miguel Arredondo <xmiguelaav@gmail.com>
2026-06-14 20:05:57 -0400

    refactor: clean up linter warnings and unused imports in auth-service and profile-service tests

--- Producto/backend/profile-service/src/test/java/com/catchandgo/profile/service/ProfileServiceTest.java
+++ Producto/backend/profile-service/src/test/java/com/catchandgo/profile/service/ProfileServiceTest.java
@@ -11,7 +11,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,6 +19,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@SuppressWarnings("null")
 public class ProfileServiceTest {
 
     @Mock
